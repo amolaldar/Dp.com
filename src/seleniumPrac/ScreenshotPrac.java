7e92f6ad03e931400783edb2ba0a3a08072a698b
Amol <amolaldar561994@gmail.com>
2023-10-09 22:34:01 +0530

    screenshot

---
+++ src/seleniumPrac/ScreenshotPrac.java
@@ -0,0 +1,23 @@
+package seleniumPrac;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.io.Files;
+
+public class ScreenshotPrac {
+
+	public static void main(String[] args) throws IOException {
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		Files.copy(src,new File("C:\\screenshot\\google.png"));
+
+	}
+
+}
